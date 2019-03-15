07fe443013b2fa6297a22a357a67434f1f601eb5
amarshukla <amarshukla123@gmail.com>
2019-03-15 16:54:59 +0530

    Update binary_search_tree.py
    Added preorder_traversal( )

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -2,7 +2,7 @@ class Node:
   """
   class to create Node
   """
-  
+
   def __init__(self, key):
     self.left = None
     self.right = None
@@ -37,6 +37,16 @@ def inorder_traversal(root):
     print(root.val)
     inorder_traversal(root.right)
 
+def preorder_traversal(root):
+  """
+  Root -> Left -> Right
+  """
+  
+  if root:
+    print(root.val)
+    preorder_traversal(root.left)
+    preorder_traversal(root.right)
+
 r = Node(50) 
 insert(r,Node(30)) 
 insert(r,Node(20)) 
@@ -45,4 +55,5 @@ insert(r,Node(70))
 insert(r,Node(60)) 
 insert(r,Node(80))
 
-inorder_traversal(r)
+#inorder_traversal(r)
+preorder_traversal(r)
